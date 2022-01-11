c0fcf753cba74d1e26f56964aa47d2847faa9f23
albatross97 <ruiwang704@gmail.com>
2022-01-11 13:28:09 -0500

    home bottle random img

--- client/src/components/modules/HomeBottle.js
+++ client/src/components/modules/HomeBottle.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import "../../utilities.css";
 import "./Bottle.css";
 import { ViewParchment } from "./Parchment.js";
+import bt1 from "../images/bt1.png";
+import bt2 from "../images/bt2.png";
+import bt3 from "../images/bt3.png";
 
 const HomeBottle = (props) => {
   const [seen, setSeen] = useState(false);
@@ -9,24 +12,13 @@ const HomeBottle = (props) => {
   const togglePop = () => {
     setSeen(!seen);
   };
+  const bts = [bt1, bt2, bt3];
+  const randombt = bts[Math.floor(Math.random() * bts.length)];
 
   return (
     <div className="u-flexColumn u-flex-alignCenter bottle">
-      <svg
-        className="bottle-icon"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="200 0 300 700"
-        onClick={togglePop}
-      >
-        <g>
-          <path
-            d="M405,302.6c0,0-24-33.2-31.4-94.2v-73.9h11.1v-11.1H375V53.7h-49.9v69.7h-9.7v11.1h11.1v73.9c-7.4,61-31.4,94.2-31.4,94.2
-    c-88.6,96.1-24,343.6-24,343.6H345l5.1-1l5.1,1H429C429,646.3,493.7,398.7,405,302.6L405,302.6z"
-          />
-        </g>
-      </svg>
-      <div className="Bottle-tag">{props.tag}</div>
+      {!seen && <img src={randombt} className="Home-bottle-icon" onClick={togglePop} />}
+      {!seen && <div className="Bottle-tag">{props.tag}</div>}
       {seen && (
         <ViewParchment
           handleClose={togglePop}
